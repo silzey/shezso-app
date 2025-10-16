cb9d55c76ea77c8a546eb262999ab5def6db1046
Kenya Mccullough <mcculloughkenya65@gmail.com>
2025-10-16 21:00:47 +0000

    Add this config // Import the functions you need from the SDKs you need

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
+
 export const firebaseConfig = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDOyZzlREsyirlpt1QkTZvDmzfILPV2Vt4",
+  authDomain: "dispensary-13773344-54196.firebaseapp.com",
+  projectId: "dispensary-13773344-54196",
+  storageBucket: "dispensary-13773344-54196.appspot.com",
+  messagingSenderId: "210293046378",
+  appId: "1:210293046378:web:7fe4f721214f946d15de13",
+  measurementId: "G-T6V7PJ4SZP"
 };
